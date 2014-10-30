a1f96fa48d8456b171987e606bfb8299974e2493
Michael Truog <mjtruog@gmail.com>
2014-10-30 14:33:22 -0700

    Fix hex package version.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Nodefinder.Mixfile do
 
   def project do
     [app: :nodefinder,
-     version: "1.3.3-rc.4",
+     version: "1.4.0-rc.4",
      language: :erlang,
      description: description,
      package: package,
